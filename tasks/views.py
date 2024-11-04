4feff87a71ca23fefbae87fea30bea612e70ae95
Luciano Figueiredo Coelho <lucianofigueiredoc@gmail.com>
2024-11-04 14:58:57 -0300

    implementado paginacao e filtros

--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,11 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        queryset = self.queryset.filter(user=self.request.user)
+        completed = self.request.query_params.get('completed')
+        if completed is not None:
+            queryset = queryset.filter(completed=(completed.lower() == 'true'))
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
